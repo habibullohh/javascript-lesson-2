c0e6ec6bdf84bb10a7ff6f63f095b323898da6c7
habibullohh <habibullohh221@gmail.com>
2022-11-11 14:05:19 +0500

    sjnvjsnis

---
+++ main.js
@@ -0,0 +1,17 @@
+alert("Bizni sayohat agentligimizni tanlaganingiz uchun raxmat✈️🌍")
+let name = prompt("Ismingizni kiriting")
+let sum = prompt("Sayohat uchun qancha pul ajratganingizni so'mda kiriting");
+let a = 1100.34;
+let b = 12354.03;
+let cost = 750 * a + 120*b;
+let c = 1000 * a * 1100.34;
+let jami = 1000 * 1100.34 - sum;
+
+if(0 > jami)(
+  document.write(jami + "hissobingizda yetarli mablag' bor sayohatga borishingiz mumkin")
+)
+else(document.write(" Sayohat uchun ajratgan pulingiz yetarli emas<br>")) 
+
+  document.write("Chipta narxi borish va qaytish uchun -500$💵" + `<br>`+ "Mehmonhona harajati(to'liq safar davomiyligi uchun -250💵)"  + `<br>`+ "Muzey va kongil ochar joylar uchun-120💶")
+
+  
